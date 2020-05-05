36fa0691bd40c636b5ec7af67509fcbef5da023e
Emmanuel <e.derozin@gmail.com>
2020-05-05 03:42:01 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   devise_for :users, controllers: {
-    confirmations: 'users/confirmations',
+    # confirmations: 'users/confirmations',
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations',
