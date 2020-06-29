cddcd38026b9dfc71346af7eb84c1b6f6428f2df
Emmanuel <e.derozin@gmail.com>
2020-06-29 02:13:43 +0200

    Update sessions_controller.rb

--- app/controllers/clients/sessions_controller.rb
+++ app/controllers/clients/sessions_controller.rb
@@ -18,7 +18,10 @@ class Clients::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || resource.user
+    end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
