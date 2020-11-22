56df44edfd462ece6eb09dc6a990d5344e5c079b
Emmanuel <e.derozin@gmail.com>
2020-11-22 18:45:41 +0100

    Update admin_user.rb

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,15 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+  has_many :admin_user_accesses
+  accepts_nested_attributes_for :admin_user_accesses, reject_if: :all_blank, allow_destroy: true
+  after_create :set_default_admin_user_accesses
+
+  def set_default_admin_user_accesses
+    ['Exercise', 'Comment', 'Dashboard'].each do |access|
+      AdminUser.last.admin_user_accesses.find_or_create_by(name: access)
+    end
+  end
 end
