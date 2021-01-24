3eab9b629f262a0cb4f61d661d16e9fe10af9769
Emmanuel <e.derozin@gmail.com>
2021-01-24 13:49:45 +0100

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     redirect_to dashboard_root_path if user_signed_in?
+    redirect_to interface_root_path if client_signed_in?
   end
 
 end
