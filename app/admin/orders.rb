22f5f535307b3fc96899bd27312d13b8b246716f
Emmanuel <e.derozin@gmail.com>
2021-01-29 00:46:45 +0100

    Update orders.rb

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,3 +1,6 @@
 ActiveAdmin.register Order do
   permit_params Order.attribute_names.map{|n| n.to_sym}
+  filter :user
+  filter :client
+  filter :status, as: :check_boxes, collection: proc { Order.statuses }
 end
